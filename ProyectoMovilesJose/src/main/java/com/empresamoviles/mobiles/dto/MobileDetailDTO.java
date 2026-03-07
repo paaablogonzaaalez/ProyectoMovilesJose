87b53cb11b152d3865661fccfdb935d6f1cc1c97
paaablogonzaaalez <pgonzalezn13@iesarroyoharnina.es>
2026-03-07 09:06:45 +0100

    Agregada MobileDetailDTO y MobileSearchCriteriaDTO MobileDetailDTO completa con los datos del móvil, MobileSearchCriteriaDTO con errores

--- ProyectoMovilesJose/src/main/java/com/empresamoviles/mobiles/dto/MobileDetailDTO.java
+++ ProyectoMovilesJose/src/main/java/com/empresamoviles/mobiles/dto/MobileDetailDTO.java
@@ -1,5 +1,28 @@
 package com.empresamoviles.mobiles.dto;
 
+import java.math.BigDecimal;
+
 public class MobileDetailDTO {
+
+	private Long id;
+    private String brand;
+    private String model;
+    private String processorType;
+    private Integer processorCores;
+    private Double processorMaxSpeedGhz;
+    private Integer storageGb;
+    private Double screenSizeInches;
+    private String screenTechnology;
+    private Integer ramGb;
+    private Double heightCm;
+    private Double widthCm;
+    private Double thicknessCm;
+    private Integer weightGrams;
+    private Integer cameraMp;
+    private Integer batteryMah;
+    private Boolean nfc;
+    private BigDecimal currentPrice;
+    
+
 
 }
--- ProyectoMovilesJose/src/main/java/com/empresamoviles/mobiles/dto/MobileSearchCriteriaDTO.java
+++ ProyectoMovilesJose/src/main/java/com/empresamoviles/mobiles/dto/MobileSearchCriteriaDTO.java
@@ -1,5 +1,24 @@
 package com.empresamoviles.mobiles.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 public class MobileSearchCriteriaDTO {
+
+	
+	private String brand;
+
+    @NotNull(message = "El precio mínimo es obligatorio")
+    @DecimalMin(value = "0.0", inclusive = true, message = "El precio mínimo debe ser mayor o igual a 0")
+    private BigDecimal priceMin;
+
+    @NotNull(message = "El precio máximo es obligatorio")
+    @Positive(message = "El precio máximo debe ser positivo")
+    priavte BigDecimal priceMax;
+
+    @Positive(message = "La RAM mínima debe ser un valor positivo")
+    private Integer ramMin;
+
 
 }
